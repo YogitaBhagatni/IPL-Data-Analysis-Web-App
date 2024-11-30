c0e6c910d23b27cd428a56363cf72f14ed51a1c6
Yogita Bhagtani <123232581+YogitaBhagatni@users.noreply.github.com>
2024-11-30 17:14:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import helper
 from PIL import Image
+from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title="IPL Data Analysis",
                    layout="wide",
